37b5eccc767da2e3b380eaaac1be99563b8bec8b
Mary Beth Hunter <mary.beth.hunter@icloud.com>
2021-08-23 21:17:17 -0500

    setup and got authors to show

--- src/javascripts/components/authors.js
+++ src/javascripts/components/authors.js
@@ -3,10 +3,15 @@
 const showAuthors = (array) => {
   document.querySelector('#store').innerHTML = '';
   // CREATE A BUTTON TO ADD BOOKS
-
+  document.querySelector('#add-button').innerHTML = '<button class="btn btn-success btn-lg mb-4" id="add-book-btn">Add A Book</button>';
   array.forEach((item) => {
     // FIXME: STUDENTS create cards for your authors
-    document.querySelector('#store').innerHTML += `${item}`;
+    document.querySelector('#store').innerHTML += `<div class="card" style="width: 18rem;">
+    <div class="card-body">
+      <h5 class="card-title">${item.first_name} ${item.last_name}</h5>
+      <h6 class="card-subtitle mb-2 text-muted">${item.email}</h6>
+    </div>
+  </div>`;
   });
 };
 
